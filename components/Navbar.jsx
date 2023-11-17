2bca5af9ae959a532265090bc87ea443475f9638
Shaurya10101 <shauryagupta8046@gmail.com>
2023-11-17 18:13:18 +0530

    First commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,35 @@
+// @ts-ignore
+
+const Navbar = () => {
+
+    return (
+        <>
+            <div>
+                <div className="flex font-poppins font-normal text-centre">One month ,on us.
+                <span className="font-bold">Download the app today for one-month trial.</span> X
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <p>Logo</p>
+                        <div>
+                            <ul className="list-none">
+                                <li><a>Features</a></li>
+                                <li><a>Plans</a></li>
+                                <li><a>Who we are</a></li>
+                                <li><a>Patnerships</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div>
+                        <p>2 icons</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default Navbar
